9120577f25cc55ecb3dac0dcfb4cd675b154f12b
zabalueva <age_eva@mail.ru>
2021-02-06 20:41:30 +0300

    feat: carbon-dating 25/25 passing

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) == 'string' && sampleActivity < 15 && sampleActivity > 0) {
+		return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (Math.log(2) / HALF_LIFE_PERIOD))
+	} else return false;
 };
