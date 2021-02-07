7639db9d7cbfbbc1bae3f172f24c15e217f984a4
zabalueva <age_eva@mail.ru>
2021-02-07 22:39:38 +0300

    feat: transform-array 3 failing

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+		throw new Error("ERR");
+	}
+
+	let result = arr.map(item => item);
+
+	for (let i = 0; i < result.length; i++) {
+		if (result[i] == '--discard-next') {
+			result.splice(i, 2);
+		}
+
+		if (result[i] == '--discard-prev') {
+			result.splice(i - 1, 2);
+		}
+
+		if (result[i] == '--double-next') {
+			result.splice(i, 1, result[i + 1] );
+		}
+
+		if (result[i] == '--double-prev') {
+			result.splice(i, 1, result[i - 1] );
+		}
+	}
+
+	return result;
 };
