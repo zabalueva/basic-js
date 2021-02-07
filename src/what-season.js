c02ae7160c5678a945fec17cd56119855295b545
zabalueva <age_eva@mail.ru>
2021-02-07 13:58:44 +0300

    feat: what-season complete

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!'
+	}
+
+  if (!(Object.prototype.toString.call(date) === '[object Date]')) {
+		throw new Error("THROWN");
+  }
+
+	let month = date.getMonth();
+
+	if (month == 2 || month == 3 || month == 4) {
+		return "spring"
+	}
+	if (month == 0 || month == 1 || month == 11) {
+		return "winter"
+	}
+	if (month == 9 || month == 8 || month == 10) {
+		return "autumn"
+	}
+	if (month == 6 || month == 5 || month == 7) {
+		return "summer"
+	}
 };
