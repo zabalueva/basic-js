264943a6c085c331772dbc8b47b86fde2f391673
zabalueva <age_eva@mail.ru>
2021-02-07 13:12:38 +0300

    feat: dream-team complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = [];
+  Array.isArray(members) &&
+    members.forEach((item) => {
+      if (typeof (item) == "string") {
+        result.push(item.trim()[0].toUpperCase());
+      }
+    })
+
+
+	return result.sort().join("") || false;
 };
