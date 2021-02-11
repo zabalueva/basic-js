0ec5675c1e3fd986881a7c291958ea3d4f2461c2
zabalueva <age_eva@mail.ru>
2021-02-11 09:40:10 +0300

    feat: extended-repeater 2 failing

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let { repeatTimes, separator = '+', addition, additionRepeatTimes, additionSeparator = '|' } = options;
+
+	return (str + (addition + additionSeparator)
+		.repeat(additionRepeatTimes)
+		.slice(0, -additionSeparator.length) + separator)
+		.repeat(repeatTimes)
+		.slice(0, -separator.length);
 };
-  
