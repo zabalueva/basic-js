76201336dd30b187853676ec78ceb6c2b4335ff9
zabalueva <age_eva@mail.ru>
2021-02-11 10:05:40 +0300

    feat: extended-repeater complete

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  let { repeatTimes, separator = '+', addition, additionRepeatTimes, additionSeparator = '|' } = options;
+  let { repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|' } = options;
+
+	if (typeof (str) !== 'string') {
+		String(str)
+	}
+
+	if (addition === undefined) {
+		return (str + separator)
+		.repeat(repeatTimes)
+		.slice(0, -separator.length);
+	}
+
+	if (typeof (addition) !== 'string') {
+		String(addition)
+	}
 
 	return (str + (addition + additionSeparator)
 		.repeat(additionRepeatTimes)
